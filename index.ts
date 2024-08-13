6a9dfa07a187737d697f07ecb392eed384dd1052
Vitor Maracajá <vitorvmrs@hotmail.com>
2024-08-13 14:21:13 -0300

    feat(default-colors): add antd colors

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { antd } from './antd';
 import { base } from './base';
 import { belt } from './belt';
 import { border } from './border';
@@ -10,6 +11,7 @@ import { opacity } from './opacity';
 import { shadow } from './shadow';
 
 export const EduzzUiTokens = {
+  antd,
   base,
   border,
   brands,
